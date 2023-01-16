3e26da9eb6a613eecf9a22250604438070a94fb4
Trip <clocktower.hlp@gmail.com>
2023-01-15 19:28:28 -0500

    Add project files.

---
+++ RuckR/Shared/Models/ProfileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuckR.Shared.Models
+{
+    public class ProfileModel
+    {
+        public string? Name { get; set; }   = null;
+        public string? Biography { get; set; } = null;
+
+    }
+}
